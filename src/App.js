c9a03121c6b0750c4d26c95a00738843e260e896
Zile028 <zile028@gmail.com>
2022-05-23 13:55:09 +0200

    add loading info

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import AllGuests from "./AllGuests";
 import "./App.css";
+import Loading from "./Loading";
 export const DeleteGuestContext = React.createContext();
 export const ChangeAttendingContext = React.createContext();
 
@@ -10,10 +11,23 @@ function App() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [allGuests, setAllGuests] = useState([]);
+  const [load, setLoad] = useState(false);
 
   useEffect(() => {
-    getAllGuests();
+    setTimeout(() => {
+      loading()
+        .then((res) => setAllGuests(res))
+        .then((res) => setLoad(true));
+    }, 1000);
   }, []);
+
+  const loading = () => {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then((res) => res.json())
+        .then((res) => resolve(res));
+    });
+  };
 
   const getAllGuests = () => {
     fetch(url)
@@ -28,9 +42,7 @@ function App() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ firstName: firstName, lastName: lastName }),
-    })
-      .then((res) => res.json())
-      .then((res) => getAllGuests());
+    }).then((res) => getAllGuests());
     setFirstName("");
     setLastName("");
   };
@@ -55,40 +67,51 @@ function App() {
 
   return (
     <div className="container py-5" data-test-id="guest">
-      {/* input data */}
-      <div className="row">
-        <div className="col-6 offset-3">
-          <input
-            className="form-control mb-3"
-            type="text"
-            placeholder="First name"
-            onInput={(e) => {
-              setFirstName(e.target.value);
-            }}
-            value={firstName}
-          />
-          <input
-            className="form-control mb-3"
-            type="text"
-            placeholder="Last name"
-            onInput={(e) => {
-              setLastName(e.target.value);
-            }}
-            value={lastName}
-          />
-          <button className="btn btn-primary form-control" onClick={addGuest}>
-            ADD
-          </button>
+      {load ? (
+        <div className="row">
+          <div className="col-6 offset-3">
+            <input
+              className="form-control mb-3"
+              type="text"
+              placeholder="First name"
+              onInput={(e) => {
+                setFirstName(e.target.value);
+              }}
+              value={firstName}
+            />
+            <input
+              className="form-control mb-3"
+              type="text"
+              placeholder="Last name"
+              onInput={(e) => {
+                setLastName(e.target.value);
+              }}
+              value={lastName}
+            />
+            <button className="btn btn-primary form-control" onClick={addGuest}>
+              ADD
+            </button>
+          </div>
+
+          {/* display data */}
+
+          {allGuests && (
+            <DeleteGuestContext.Provider value={deleteGuest}>
+              <ChangeAttendingContext.Provider value={changeAttending}>
+                {allGuests.length > 0 ? (
+                  <AllGuests guests={allGuests} />
+                ) : (
+                  <div className="col-6 offset-3 text-center mt-3">
+                    <p>You dont have any guest on list.</p>
+                  </div>
+                )}
+              </ChangeAttendingContext.Provider>
+            </DeleteGuestContext.Provider>
+          )}
         </div>
-        {/* display data */}
-        {allGuests && (
-          <DeleteGuestContext.Provider value={deleteGuest}>
-            <ChangeAttendingContext.Provider value={changeAttending}>
-              <AllGuests guests={allGuests} />
-            </ChangeAttendingContext.Provider>
-          </DeleteGuestContext.Provider>
-        )}
-      </div>
+      ) : (
+        <Loading />
+      )}
     </div>
   );
 }
---
+++ src/Loading.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <div className="col-6.offset-3 text-center">
+      <img
+        src="https://galeri13.uludagsozluk.com/745/islam-slimani_1807491.gif"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default Loading;
